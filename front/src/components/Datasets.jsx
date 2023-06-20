677ce2f9c99db33b83b7ab0077643288841ba560
Aleksandr Ledovskiy <adledovskiy@avito.ru>
2023-06-21 00:21:06 +0300

    updated datasets page

--- front/src/components/Datasets.jsx
+++ front/src/components/Datasets.jsx
@@ -3,7 +3,10 @@ function Datasets (props) {
     <tr key={dataset.id.toString()}>
       <td className="border px-4 py-2"><a className="text-indigo-500" href={`/datasets/${dataset.id}`}>{dataset.full_name}</a></td>
       <td className="border px-4 py-2">{dataset.stats ? dataset.stats.n_components : null}</td>
-      <td className="border px-4 py-2">{dataset.stats ? dataset.stats.agreement : null}</td>
+      <td className="border px-4 py-2">{dataset.stats ? dataset.stats.n_annotations : null}</td>
+      <td className="border px-4 py-2">{dataset.stats ? dataset.stats.n_components_with_annotations : null}</td>
+      <td className="border px-4 py-2">{dataset.stats ? dataset.stats.n_components_with_images : null}</td>
+      <td className="border px-4 py-2">{dataset.stats ? dataset.stats.n_users : null}</td>
     </tr>
   );
   return (
@@ -13,8 +16,11 @@ function Datasets (props) {
         <thead>
           <tr>
             <th className="px-4 py-2">Dataset</th>
-            <th className="px-4 py-2">N components</th>
-            <th className="px-4 py-2">Agreement</th>
+            <th className="px-4 py-2">N ICs</th>
+            <th className="px-4 py-2">N an-s</th>
+            <th className="px-4 py-2">N ICs with an-s</th>
+            <th className="px-4 py-2">N ICs with images</th>
+            <th className="px-4 py-2">N annotators</th>
           </tr>
         </thead>
         <tbody>
